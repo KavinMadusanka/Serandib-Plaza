5930cd81e8ce2cb22692638217904ef4215521c6
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-08-19 19:33:33 +0530

    Updated folder structure

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+
+const connectDB = async() =>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white)
+    }catch(error){
+        console.log(`Error in Mongodb ${error}`.bgRed.white)
+    }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,20 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middelwares
+app.use(express.json())
+app.use(morgan('dev'))
 
 // rest api
 app.get("/" , (req,res) => {
