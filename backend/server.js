27f0bae20b0798339db84d9b63a42751b05dc5ca
Navishka Gunawardana <128272519+it22570444@users.noreply.github.com>
2024-08-25 09:31:35 +0530

    product CRUD

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,15 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+<<<<<<< Updated upstream
 import categoryRoutes from './routes/categoryRoute.js'
+=======
+import AuthRoutes from './routes/AuthRoute.js'
+import promotionRoutes from "./routes/promotionRoute.js"
+import categoryRoutes from './routes/categoryRoute.js'
+import productRoutes from  './routes/productRoute.js'
+
+>>>>>>> Stashed changes
 
 //configure env
 dotenv.config();
@@ -19,7 +27,14 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 //routes
+<<<<<<< Updated upstream
 app.use('/api/v1/category', categoryRoutes);
+=======
+app.use("/api/v1/userauth",AuthRoutes)
+app.use("/api/v1/promotions",promotionRoutes)
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes);
+>>>>>>> Stashed changes
 
 // rest api
 app.get("/" , (req,res) => {
