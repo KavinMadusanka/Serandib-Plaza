d7037c1007f6877066128e8d87859b1291a5d810
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-08-28 18:13:27 +0530

    Update fronted

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header1 from './Header1'
 import Footer1 from './Footer1'
+import { Helmet } from "react-helmet"
 
-const Layout = ({children}) => {
+const Layout = ({children, title,description,keywords,author}) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <meta name = "description" content = { description } />
+        <meta name = "keywords" content = { keywords } />
+        <meta name = "author" content = { author } />
+        <title>{ title }</title>
+      </Helmet>
         <Header1/>
         <main style={{minHeight: '100vh'}}>
             {children}
@@ -12,6 +20,13 @@ const Layout = ({children}) => {
         <Footer1/>
     </div>
   )
+}
+
+Layout.defaultProps = {
+  title: "Serendib Plaza - Shopping mall app",
+  description: "Mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "SE-S1-WD-04"
 }
 
 export default Layout
