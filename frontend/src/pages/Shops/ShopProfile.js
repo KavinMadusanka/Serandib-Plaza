57159866d3a507ffca5eb2bec98e7ae1768899b7
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-08-30 13:51:42 +0530

    Created frontend shop profile details display part

--- frontend/src/pages/Shops/ShopProfile.js
+++ frontend/src/pages/Shops/ShopProfile.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+//import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/auth';
+
 
 const ShopProfile = () => {
-  return (
-    <div>ShopProfile</div>
-  )
-}
+    const [auth] = useAuth();
 
-export default ShopProfile
+    return (
+       
+            <div className='profile'>
+                <h1>{auth?.shopOwner?.shopname} </h1>
+                {/* <h1><strong>Shop Name:</strong> {auth?.shopOwner?.shopname}</h1> */}
+                <h2>Owner Details</h2>
+                <p><strong>Owner Name:</strong> {auth?.shopOwner?.fullname}</p>
+                <p><strong>Owner Email:</strong> {auth?.shopOwner?.owner_email}</p>
+                <p><strong>Owner Contact Number:</strong> {auth?.shopOwner?.owner_contact}</p>
+                <p><strong>Owner NIC:</strong> {auth?.shopOwner?.nic}</p>
+                <p><strong>Business Registration Number:</strong> {auth?.shopOwner?.businessregno}</p>
+                <p><strong>Tax ID Number:</strong> {auth?.shopOwner?.tax_id_no}</p>
+
+                <h2>Shop Details</h2>
+                <p><strong>Email:</strong> {auth?.shopOwner?.email}</p>
+                <p><strong>Contact Number:</strong> {auth?.shopOwner?.shopcontact}</p>
+                <p><strong>Business Type:</strong> {auth?.shopOwner?.businesstype}</p>
+                <p><strong>Category:</strong> {auth?.shopOwner?.category}</p>
+                <p><strong>Description:</strong> {auth?.shopOwner?.description}</p>
+                <p><strong>Operating Hours:</strong> {auth?.shopOwner?.operating_hrs_from} - {auth?.shopOwner?.operating_hrs_to}</p>
+                <p><strong>Location:</strong> {auth?.shopOwner?.shoplocation}</p>
+            </div>
+       
+    );
+};
+
+export default ShopProfile;
