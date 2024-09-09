73450e65c4e64fe14fead64792480cd2965b199c
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-09-10 00:36:22 +0530

    Updated promotion frontend from shop side

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import { Routes, Route} from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import Pagenotfound from "./pages/Pagenotfound";
+import RegisterUser from "./pages/Auth/RegisterUser";
+import RegisterShop from "./pages/Auth/RegisterShop";
+import Registration from "./pages/Auth/Registration";
+//import { ToastContainer} from "react-toastify";
+//import 'react-toastify/dist/ReactToastify.css'
+import Login from "./pages/Auth/Login";
+import ShopProfile from "./pages/Shops/ShopProfile";
+
+import ProfileUser from './pages/User/ProfileUser';
+import UpdateShopProfile from "./pages/Shops/UpdateShopProfile";
+import UpdateUserProfile from "./pages/User/UpdateUserProfile";
+
+
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import ContactUs from "./pages/ContactUs";
+import AllPromotions from "./pages/Promotions";
+import AllUsers from "./pages/Admin/AllUsers";
+import AllShops from "./pages/Admin/AllShops";
+import AddPromotion from "./pages/Shops/AddPromotion";
+import DisplayProductpage from "./pages/DisplayProductpage";
+import AllPromo from "./pages/Shops/AllPromo";
+import UpdatePromotion from "./pages/Shops/UpdatePromotion";
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<Registration/>} />
+      <Route path="/registerUser" element={<RegisterUser/>} />
+      <Route path="/registerShop" element={<RegisterShop/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/shopProfile" element={<ShopProfile />} />
+      <Route path="*" element={<Pagenotfound />} />
+      <Route path="/userprofile" element={<ProfileUser />} />
+      <Route path="/updateShopProfile" element={<UpdateShopProfile />} />
+      <Route path="/updateprofile" element={<UpdateUserProfile />}/>
+      <Route path="/adminProfile" element={<AdminDashboard />} />
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="/promotions" element={<AllPromotions />} />
+      <Route path="/allusers" element={<AllUsers />} />
+      <Route path="/allshops" element={<AllShops />} />
+      <Route path="/addpromotions" element={<AddPromotion />} />
+      <Route path="/allpromo" element={<AllPromo />} />
+      <Route path="/updatePromotion" element={<UpdatePromotion />} />
+
+      <Route path="/displayProductpage" element={<DisplayProductpage/>} />
+
+    </Routes>
+    
+     
+    </>
+  );
+}
+
+export default App;
