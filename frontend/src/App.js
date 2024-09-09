c6ec6281a5a1027e12c11ea2ab211ab59cc0bfe7
Navishka Gunawardana <128272519+it22570444@users.noreply.github.com>
2024-09-10 03:42:17 +0530

    Category FrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
+<<<<<<< Updated upstream
 // import logo from './logo.svg';
 import './App.css';
+=======
+import { Routes, Route} from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import Pagenotfound from "./pages/Pagenotfound";
+import RegisterUser from "./pages/Auth/RegisterUser";
+import RegisterShop from "./pages/Auth/RegisterShop";
+import Registration from "./pages/Auth/Registration";
+//import { ToastContainer} from "react-toastify";
+//import 'react-toastify/dist/ReactToastify.css'
+import Login from "./pages/Auth/Login";
+import ShopProfile from "./pages/Shops/ShopProfile";
+
+import ProfileUser from './pages/User/ProfileUser';
+import UpdateShopProfile from "./pages/Shops/UpdateShopProfile";
+import UpdateUserProfile from "./pages/User/UpdateUserProfile";
+
+import { CreateCategory } from './pages/Shops/CreateCategory';
+
+
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import ContactUs from "./pages/ContactUs";
+import AllPromotions from "./pages/Promotions";
+import AllUsers from "./pages/Admin/AllUsers";
+import AllShops from "./pages/Admin/AllShops";
+import AddPromotion from "./pages/Shops/AddPromotion";
+
+
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
     {/*  <div className="App">
        <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +50,31 @@ function App() {
          </a>
        </header>
      </div> */}
+=======
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<Registration/>} />
+      <Route path="/registerUser" element={<RegisterUser/>} />
+      <Route path="/registerShop" element={<RegisterShop/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/shopProfile" element={<ShopProfile />} />
+      <Route path="*" element={<Pagenotfound />} />
+      <Route path="/userprofile" element={<ProfileUser />} />
+      <Route path="/updateShopProfile" element={<UpdateShopProfile />} />
+      <Route path="/updateprofile" element={<UpdateUserProfile />}/>
+      <Route path="/adminProfile" element={<AdminDashboard />} />
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="/promotions" element={<AllPromotions />} />
+      <Route path="/allusers" element={<AllUsers />} />
+      <Route path="/allshops" element={<AllShops />} />
+      <Route path="/addpromotions" element={<AddPromotion />} />
+
+      <Route path="/createCategory" element={<CreateCategory/>}/>
+
+    </Routes>
+    
+     
+>>>>>>> Stashed changes
     </>
   );
 }
