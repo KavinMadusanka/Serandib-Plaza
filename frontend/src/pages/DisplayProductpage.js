759f50849370b44a8ed424aa832d3be7c674b77b
Prabhasari <it22227140@my.sliit.lk>
2024-09-10 21:51:45 +0530

    created shoppingcart updated part

--- frontend/src/pages/DisplayProductpage.js
+++ frontend/src/pages/DisplayProductpage.js
@@ -6,6 +6,10 @@ import axios from "axios";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
 
+// Utility function to format price in LKR
+const formatPrice = (price) => {
+    return new Intl.NumberFormat('en-LK', { style: 'currency', currency: 'LKR' }).format(price);
+};
 
 const DisplayProductpage = () => {
     const [auth, setAuth] = useAuth();
@@ -13,12 +17,11 @@ const DisplayProductpage = () => {
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
 
-
-    //get all category
+    // Get all categories
     const getAllCategory = async () => {
         try {
-            const {data} = await axios.get("/api/v1/category/get-category");
-            if (data?.success){
+            const { data } = await axios.get("/api/v1/category/get-category");
+            if (data?.success) {
                 setCategories(data?.category);
             }
         } catch (error) {
@@ -28,15 +31,14 @@ const DisplayProductpage = () => {
 
     useEffect(() => {
         getAllCategory();
-    },[]);
+    }, []);
 
-
-    //get products
+    // Get products
     const getAllProducts = async () => {
-        try{
-            const {data} = await axios.get("/api/v1/product/get-product");
+        try {
+            const { data } = await axios.get("/api/v1/product/get-product");
             setProducts(data.products);
-        }catch(error){
+        } catch (error) {
             console.log(error);
         }
     };
@@ -45,9 +47,7 @@ const DisplayProductpage = () => {
         getAllProducts();
     }, []);
 
-
-
-    return(
+    return (
         <Layout title={"All Products - Best offers"}>
             <div className="row mt-3">
                 <div className="col-md-3">
@@ -63,41 +63,42 @@ const DisplayProductpage = () => {
                 <div className="col-md-9">
                     <h1 className="text-center">All Products</h1>
                     <div className="d-flex flex-wrap">
-                    {products.map((p) => (
-                        <div className="card m-2" style={{width: "18rem"}}>
-                            <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
-                            className="card-img-top"
-                            alt={p.name}
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">{p.name}</h5>
-                                <p className="card-text">{p.description}</p>
-                                <button className="btn btn-primary ms-1">More Details</button>
-                                <button className="btn btn-secondary ms-1"
-                                onClick={() => {
-                                    setCart([...cart, p]);
-                                    localStorage.setItem(
-                                        "cart",
-                                        JSON.stringify([...cart, p])
-                                    );
-                                    toast.success("Item Added to cart");
-                                }}>
-                                    ADD TO CART
+                        {products.map((p) => (
+                            <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
+                                <img
+                                    src={`/api/v1/product/product-photo/${p._id}`}
+                                    className="card-img-top"
+                                    alt={p.name}
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{p.name}</h5>
+                                    <p className="card-text">{p.description}</p>
+                                    <p className="card-text">{formatPrice(p.price)}</p>
+                                    <button className="btn btn-primary ms-1">More Details</button>
+                                    <button
+                                        className="btn btn-secondary ms-1"
+                                        onClick={() => {
+                                            setCart([...cart, p]);
+                                            localStorage.setItem(
+                                                "cart",
+                                                JSON.stringify([...cart, p])
+                                            );
+                                            toast.success("Item Added to cart");
+                                        }}
+                                    >
+                                        ADD TO CART
                                     </button>
-                                <button class="btn btn-thirshary ms-1" >
-                                    ADD TO Wishlist
+                                    <button className="btn btn-thirshary ms-1">
+                                        ADD TO Wishlist
                                     </button>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                        
+                        ))}
                     </div>
                 </div>
             </div>
         </Layout>
     );
-
 };
 
 export default DisplayProductpage;
