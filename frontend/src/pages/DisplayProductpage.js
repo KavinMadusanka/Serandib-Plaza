c8a7f5aec8d6d13ede19d540eecf73f601e0fc89
Prabhasari <piyushaprabhasari@gmail.com>
2024-09-25 08:57:07 +0530

    created price and category filters

--- frontend/src/pages/DisplayProductpage.js
+++ frontend/src/pages/DisplayProductpage.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
-import { Checkbox } from "antd";
-import { useAuth } from "../context/auth";
+import { Checkbox, Radio } from "antd";
 import axios from "axios";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
+import { Prices } from "../components/Prices";
 
 // Utility function to format price in LKR
 const formatPrice = (price) => {
@@ -12,10 +12,13 @@ const formatPrice = (price) => {
 };
 
 const DisplayProductpage = () => {
-    const [auth, setAuth] = useAuth();
+    
     const [cart, setCart] = useCart();
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [checked, setChecked] = useState([]);
+    const [radio, setRadio] = useState([]);
+
 
     // Get all categories
     const getAllCategory = async () => {
@@ -43,9 +46,39 @@ const DisplayProductpage = () => {
         }
     };
 
+    //filter by category
+    const handleFilter = (value, id) => {
+        let all = [...checked];
+        if (value) {
+            all.push(id);
+        }else {
+            all = all.filter((c) => c !== id);
+        }
+        setChecked(all);
+    };
+
     useEffect(() => {
-        getAllProducts();
-    }, []);
+        if (!checked.length || !radio.length) getAllProducts();
+    }, [checked.length, radio.length]);
+
+    useEffect(() => {
+        if (checked.length || radio.length) filterProduct();
+    }, [checked, radio]);
+
+
+    //get filtered product
+    const filterProduct = async () => {
+        try {
+            const { data } = await axios.post("/api/v1/product/product-filters", {
+                checked,
+                radio,
+            });
+            setProducts(data?.products);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return (
         <Layout title={"All Products - Best offers"}>
@@ -54,13 +87,29 @@ const DisplayProductpage = () => {
                     <h4 className="text-center">Filter By Category</h4>
                     <div className="d-flex flex-column">
                         {categories?.map((c) => (
-                            <Checkbox key={c._id} onChange={(e) => console.log(e)}>
+                            <Checkbox key={c._id} 
+                            onChange={(e) => handleFilter(e.target.checked,c._id)}
+                            >
                                 {c.name}
                             </Checkbox>
                         ))}
                     </div>
+               
+                 {/* price filter  */}
+                <h4 className="text-center mt-4">Filter By Price</h4>
+                <div className="d-flex flex-column">
+                    <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                        {Prices?.map((p) => (
+                            <div key={p._id}>
+                                <Radio value={p.array}>{p.name}</Radio>
+                            </div>
+                        ))}
+                        </Radio.Group>
                 </div>
+            </div>
+
                 <div className="col-md-9">
+                   
                     <h1 className="text-center">All Products</h1>
                     <div className="d-flex flex-wrap">
                         {products.map((p) => (
@@ -72,7 +121,7 @@ const DisplayProductpage = () => {
                                 />
                                 <div className="card-body">
                                     <h5 className="card-title">{p.name}</h5>
-                                    <p className="card-text">{p.description}</p>
+                                    <p className="card-text">{p.description.substring(0, 30)}...</p>
                                     <p className="card-text">{formatPrice(p.price)}</p>
                                     <button className="btn btn-primary ms-1">More Details</button>
                                     <button
