2de31bb7c7d79660d3aef7d2aa35e315ef9d2213
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-01 14:07:33 +0530

    Update eventForm.js

--- frontend/src/components/Form/eventForm.js
+++ frontend/src/components/Form/eventForm.js
@@ -6,20 +6,28 @@ import { useNavigate } from 'react-router-dom';
 import {} from '../style/EventForm.css'
 
 const EventForm = () => { // Updated the component name to start with an uppercase letter
-    const [fullName, setFullName] = useState("");
-    const [address, setAddress] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
+    const [title, setTitle] = useState("");
+    const [venue, setVenue] = useState("");
     const [email, setEmail] = useState("");
-    const [selectedDate, setSelectedDate] = useState("");
-    const [selectedTime, setSelectedTime] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [image,setImage] = useState("");
+    const [auth,setAuth] = useAuth();
     const navigate = useNavigate();
-    const [userId, setUserId] = useState("");
 
-    const handleFullNameChange = (e) => {
-        const input = e.target.value;
-        const regex = /^[A-Za-z.\s]*$/; // Allow only letters, spaces, and periods
-        if (regex.test(input)) {
-            setFullName(input);
+    useEffect(() => {
+        if (auth && auth.user) {
+            setEmail(auth.user.email);
+        }
+      }, [auth]);
+
+    //only gets alpherbatds
+    const handleKeyPress = (event) => {
+        const regex = /^[a-zA-Z\s]*$/;
+        if(!regex.test(event.key)){
+        event.preventDefault();
         }
     };
 
@@ -34,27 +42,34 @@ const EventForm = () => { // Updated the component name to start with an upperca
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            if (phoneNumber.length !== 10) {
-                toast.error("Contact number must be 10 characters long");
-                return;
-            }
-            const res = await axios.post("/api/v1/appointment/appointment1", {
-                fullName,
-                address,
-                phoneNumber,
-                email,
-                selectedDate,
-                selectedTime,
-                userId,
-            });
+            const NewEvent = new FormData();
+            NewEvent.append("title", title);
+            NewEvent.append("venue", venue);
+            NewEvent.append("email", email);
+            NewEvent.append("startDate", startDate);
+            NewEvent.append("endDate", endDate);
+            NewEvent.append("startTime", startTime);
+            NewEvent.append("endTime", endTime);
+            NewEvent.append("image", image);
+            const res = await axios.post("/api/v1/Event/addNewEvent", NewEvent);
+            // if(res?.success){
+            //     toast.success(res.message);
+            //     setTitle('');
+            //     setEmail('');
+            //     setStartDate('');
+            //     setEndDate('');
+            //     setStartTime('');
+            //     setEndTime('');
+            //     setImage(null);
+            //   }
             if (res && res.data.success) {
                 toast.success(res.data.message);
-                navigate('/myappointments');
+                navigate('/EventAdmin');
             } else {
                 toast.error(res.data.message);
             }
         } catch (error) {
-            toast.error('Something went wrong');
+            toast.error('Something went wrong adding new event');
         }
     };
 
@@ -73,9 +88,10 @@ const EventForm = () => { // Updated the component name to start with an upperca
                                         <label htmlFor="fullName">Event Title :</label>
                                         <input
                                             type="text"
-                                            value={fullName}
-                                            onChange={handleFullNameChange}
+                                            value={title}
+                                            onChange={(e) => setTitle(e.target.value)}
                                             placeholder="Enter your full name"
+                                            onKeyPress={handleKeyPress}
                                             required
                                         />
                                     </div>
@@ -84,25 +100,36 @@ const EventForm = () => { // Updated the component name to start with an upperca
                                         <label htmlFor="address">Venue :</label>
                                         <input
                                             type="text"
-                                            value={address}
-                                            onChange={(e) => setAddress(e.target.value)}
+                                            value={venue}
+                                            onChange={(e) => setVenue(e.target.value)}
                                             placeholder="Enter your address"
+                                            // onKeyPress={handleKeyPress}
                                             required
                                         />
                                     </div>
 
-                                    <div className="form-group">
-                                        <label htmlFor="phoneNumber">Phone Number:</label>
-                                        <input
-                                            type="tel"
-                                            value={phoneNumber}
-                                            onChange={(e) => {
-                                                const newValue = e.target.value.replace(/[^\d]/g, '');
-                                                setPhoneNumber(newValue);
-                                            }}
-                                            placeholder="Enter your phone number"
-                                            required
-                                        />
+                                    {/* Photo Upload */}
+                                    <div className='uploadbox'>
+                                        <label className="btn btn-outline-secondary col-md-12">
+                                            {image ? image.name : "Upload Photo"}
+                                            <input
+                                                type="file"
+                                                name="image"
+                                                accept="image/*"
+                                                onChange={(e) => setImage(e.target.files[0])}
+                                                hidden
+                                            />
+                                        </label>
+                                    </div>
+                                    {/* Display Uploaded Photo */}
+                                    <div>
+                                        <div className="mb-3">
+                                            {image && (
+                                                <div className="text-center">
+                                                <img src={URL.createObjectURL(image)} alt="LOst or Found Image" height={"200px"} className="img img-responsive" />
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
 
                                     <table style={{ width: '100%' }}>
@@ -110,23 +137,48 @@ const EventForm = () => { // Updated the component name to start with an upperca
                                             <tr>
                                                 <td style={{ paddingRight: '2%' }}>
                                                     <div className="form-group">
-                                                        <label htmlFor="selectedDate">Select Date:</label>
+                                                        <label htmlFor="selectedDate">Select Start Date:</label>
                                                         <input
                                                             type="date"
-                                                            value={selectedDate}
-                                                            onChange={(e) => setSelectedDate(e.target.value.split('T')[0])}
+                                                            value={startDate}
+                                                            onChange={(e) => setStartDate(e.target.value.split('T')[0])}
                                                             min={getTodayDate()}
+                                                            required
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td style={{ paddingRight: '2%' }}>
+                                                    <div className="form-group">
+                                                        <label htmlFor="selectedDate">Select End Date:</label>
+                                                        <input
+                                                            type="date"
+                                                            value={endDate}
+                                                            onChange={(e) => setEndDate(e.target.value.split('T')[0])}
+                                                            min={getTodayDate()}
+                                                            required
+                                                        />
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div className="form-group">
+                                                        <label htmlFor="selectedTime">Select Start Time:</label>
+                                                        <input
+                                                            type="time"
+                                                            value={startTime}
+                                                            onChange={(e) => setStartTime(e.target.value)}
                                                             required
                                                         />
                                                     </div>
                                                 </td>
                                                 <td>
                                                     <div className="form-group">
-                                                        <label htmlFor="selectedTime">Select Time:</label>
+                                                        <label htmlFor="selectedTime">Select End Time:</label>
                                                         <input
                                                             type="time"
-                                                            value={selectedTime}
-                                                            onChange={(e) => setSelectedTime(e.target.value)}
+                                                            value={endTime}
+                                                            onChange={(e) => setEndTime(e.target.value)}
                                                             required
                                                         />
                                                     </div>
@@ -134,6 +186,10 @@ const EventForm = () => { // Updated the component name to start with an upperca
                                             </tr>
                                         </tbody>
                                     </table>
+                                </div>
+                                <br></br>
+                                <div className="form-buttons">
+                                    <button type="submit">Add Event</button>
                                 </div>
                             </div>
                         </div>
