6d0e1615aeb90c034cabe32eb2beae3082783382
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-01 21:23:21 +0530

    Update eventModel.js

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -7,11 +7,11 @@ const eventModel = new mongoose.Schema(
             required: true
         },
         startDate: {
-            type: Date,
+            type: String,
             required:true
         },
         endDate:{
-            type:Date,
+            type:String,
             required:true
         },
         image:{
